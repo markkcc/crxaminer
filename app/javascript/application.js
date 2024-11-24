1848f4f8528ad4c5f45f1121b011dd9c2d51a00d
markkcc <mfkhoury@gmail.com>
2024-11-24 16:36:02 -0500

    Remove unnecessary JavaScript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import { Application } from "@hotwired/stimulus"
-import { registerControllers } from "@hotwired/stimulus-loading"
+//import "@hotwired/turbo-rails"
+//import { Application } from "@hotwired/stimulus"
+//import { registerControllers } from "@hotwired/stimulus-loading"
 
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-registerControllers(application, context) 
+//const application = Application.start()
+//const context = require.context("./controllers", true, /\.js$/)
+//registerControllers(application, context) 
--- app/javascript/controllers/analysis_controller.js
+++ app/javascript/controllers/analysis_controller.js
@@ -1,18 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+//import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
-  connect() {
-    if (this.element.dataset.autostart === "true") {
-      this.startAnalysis()
-    }
-  }
+//export default class extends Controller {
+//  connect() {
+//    if (this.element.dataset.autostart === "true") {
+//      this.startAnalysis()
+//    }
+//  }
 
-  startAnalysis() {
-    const extensionId = this.element.dataset.extensionId
-    fetch(`/welcome?id=${extensionId}&analyze=true`, {
-      headers: {
-        "Accept": "text/vnd.turbo-stream.html"
-      }
-    })
-  }
-} 
+//  startAnalysis() {
+//    const extensionId = this.element.dataset.extensionId
+//    fetch(`/welcome?id=${extensionId}&analyze=true`, {
+//      headers: {
+//        "Accept": "text/vnd.turbo-stream.html"
+//      }
+//    })
+//  }
+//} 
